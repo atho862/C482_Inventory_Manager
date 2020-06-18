85fb3d827636badb070fe6f75e85ee013abcbdd5
atho862 <54923852+atho862@users.noreply.github.com>
2020-06-17 23:05:44 -0500

    Add initialization of inventory data

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import DataProvider.Inventory;
+import Models.InHouse;
+import Models.Part;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,14 +11,20 @@ public class Main extends Application {
 
 @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/Views/Main.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Inventory Manager");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
+
+        InHouse partOne = new InHouse(1, "Test Part", 50.00, 5, 1, 5, 1);
+        InHouse partTwo = new InHouse(2, "Test Part 1", 75.00, 10, 1, 10, 2);
+        Inventory.addPart(partOne);
+        Inventory.addPart(partTwo);
+
         launch(args);
     }
 }
