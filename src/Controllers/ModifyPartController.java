665656a5f09e7d1fd76b282b52938fdbfdb12c10
atho862 <54923852+atho862@users.noreply.github.com>
2020-06-18 22:36:47 -0500

    Add skeleton code to ModifyPartController

--- src/Controllers/ModifyPartController.java
+++ src/Controllers/ModifyPartController.java
@@ -1,4 +1,75 @@
 package Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class ModifyPartController {
+    @FXML
+    private RadioButton btnRadioOutsourced;
+
+    @FXML
+    private TextField txtMin;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtCost;
+
+    @FXML
+    private ToggleGroup btnRadioPartType;
+
+    @FXML
+    private Label lblMax;
+
+    @FXML
+    private TextField txtMachineId;
+
+    @FXML
+    private TextField txtInventory;
+
+    @FXML
+    private Label lblId;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private Label lblMachineId;
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private Label lblName;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private TextField txtMax;
+
+    @FXML
+    private RadioButton btnRadioInHouse;
+
+    @FXML
+    private Label lblInventory;
+
+    @FXML
+    private Label lblPrice;
+
+    @FXML
+    private Label lblMin;
+
+    @FXML
+    void onActionSave(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionCancel(ActionEvent event) {
+
+    }
+
 }
