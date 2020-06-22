682993e93bfbef016296847edb850da625563d25
atho862 <54923852+atho862@users.noreply.github.com>
2020-06-21 23:26:38 -0500

    implement methods in add product controller

--- src/Controllers/AddProductController.java
+++ src/Controllers/AddProductController.java
@@ -1,15 +1,30 @@
 package Controllers;
 
+import DataProvider.Inventory;
 import Models.Part;
+import Models.Product;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AddProductController implements Initializable {
+
+    private ObservableList<Part> associatedParts;
+    private Stage stage;
+    private Parent root;
+
     @FXML
     private TextField txtMin;
 
@@ -95,13 +110,30 @@ public class AddProductController implements Initializable {
     private TextField txtPrice;
 
     @FXML
-    void onActionSaveBtn(ActionEvent event) {
+    void onActionSaveBtn(ActionEvent event) throws IOException {
+        Product product = new Product(Integer.valueOf(txtId.getText()),
+                txtName.getText(),
+                Double.valueOf(txtPrice.getText()),
+                Integer.valueOf(txtInventory.getText()),
+                Integer.valueOf(txtMin.getText()),
+                Integer.valueOf(txtMax.getText()),
+                associatedParts);
+        Inventory.addProduct(product);
 
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("/Views/Main.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
-    void onActionCancelBtn(ActionEvent event) {
-
+    void onActionCancelBtn(ActionEvent event) throws IOException {
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("/Views/Main.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
@@ -111,16 +143,40 @@ public class AddProductController implements Initializable {
 
     @FXML
     void onActionAddBtn(ActionEvent event) {
-
+        Part part = tblViewAddAssociatedPart.getSelectionModel().getSelectedItem();
+        associatedParts.add(part);
     }
 
     @FXML
     void onActionDeleteBtn(ActionEvent event) {
+        Part part = tblViewAssociatedParts.getSelectionModel().getSelectedItem();
+        associatedParts.remove(part);
+    }
 
+    public void sendProductId(int id){
+        txtId.setText(String.valueOf(id));
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        initializeAddAssociatedPartsTable();
+        initializeAssociatedPartsTable();
+    }
 
+    private void initializeAddAssociatedPartsTable(){
+        tblViewAddAssociatedPart.setItems(Inventory.getAllParts());
+        tblColumnAddPartId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblColumnAddPartName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tblColumnAddInventory.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        tblColumnAddPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+    }
+
+    private void initializeAssociatedPartsTable(){
+        associatedParts = FXCollections.observableArrayList();
+        tblViewAssociatedParts.setItems(associatedParts);
+        tblColumnPartId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblColumnPartName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tblColumnInventory.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        tblColumnPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
     }
 }
