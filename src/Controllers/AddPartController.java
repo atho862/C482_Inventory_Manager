a88be38de4e25690bd827bbcc28100c867711527
atho862 <54923852+atho862@users.noreply.github.com>
2020-06-21 23:26:21 -0500

    implement methods in add part controller

--- src/Controllers/AddPartController.java
+++ src/Controllers/AddPartController.java
@@ -1,13 +1,26 @@
 package Controllers;
 
+import DataProvider.Inventory;
+import Models.InHouse;
+import Models.Outsourced;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AddPartController implements Initializable {
+
+    Stage stage;
+    Parent root;
 
     @FXML
     private RadioButton btnRadioOutsourced;
@@ -67,17 +80,68 @@ public class AddPartController implements Initializable {
     private Label lblMin;
 
     @FXML
-    void onActionSave(ActionEvent event) {
+    void onActionSave(ActionEvent event) throws IOException {
+        if (btnRadioInHouse.isSelected()){
+            InHouse part = new InHouse(Integer.valueOf(txtId.getText()),
+                    txtName.getText(),
+                    Double.valueOf(txtCost.getText()),
+                    Integer.valueOf(txtInventory.getText()),
+                    Integer.valueOf(txtMin.getText()),
+                    Integer.valueOf(txtMax.getText()),
+                    Integer.valueOf(txtMachineId.getText()));
+            Inventory.addPart(part);
+        }
+        else {
+            Outsourced part = new Outsourced(Integer.valueOf(txtId.getText()),
+                    txtName.getText(),
+                    Double.valueOf(txtCost.getText()),
+                    Integer.valueOf(txtId.getText()),
+                    Integer.valueOf(txtMin.getText()),
+                    Integer.valueOf(txtMin.getText()),
+                    txtMachineId.getText());
+            Inventory.addPart(part);
+        }
 
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("/Views/Main.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
-    void onActionCancel(ActionEvent event) {
+    void onActionCancel(ActionEvent event) throws IOException {
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("/Views/Main.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
+    public void sendPartId(int id){
+        txtId.setText(String.valueOf(id));
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        btnRadioInHouse.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (newValue) {
+                    lblMachineId.setText("Machine Id");
+                    txtMachineId.setPromptText("Machine Id");
+                }
 
+            }
+        });
+
+        btnRadioOutsourced.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (newValue)
+                lblMachineId.setText("Company Name");
+                txtMachineId.setPromptText("Company Name");
+            }
+        });
     }
 }
