4974c36ef76fa09cff8ba840bedab3a2b486911b
atho862 <54923852+atho862@users.noreply.github.com>
2020-06-21 23:26:59 -0500

    implemented onAction methods on main controller

--- src/Controllers/MainController.java
+++ src/Controllers/MainController.java
@@ -3,15 +3,25 @@ package Controllers;
 import DataProvider.Inventory;
 import Models.Part;
 import Models.Product;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+
+    Stage stage;
+    Parent root;
 
     @FXML
     private TableColumn<Part, Integer> tblColumnPartId;
@@ -88,18 +98,44 @@ public class MainController implements Initializable {
     }
 
     @FXML
-    void onActionAddProduct(ActionEvent event) {
+    void onActionAddProduct(ActionEvent event) throws IOException {
 
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/Views/AddProduct.fxml"));
+        loader.load();
+
+        AddProductController controller = loader.getController();
+        controller.sendProductId(getNextProductId());
+
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        Parent scene = loader.getRoot();
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @FXML
-    void onActionModifyProduct(ActionEvent event) {
+    void onActionModifyProduct(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/Views/ModifyProduct.fxml"));
+        loader.load();
 
+        ModifyProductController controller = loader.getController();
+        controller.sendProduct(tblProducts.getSelectionModel().getSelectedItem());
+
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        root = loader.getRoot();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
     void onActionDeleteProduct(ActionEvent event) {
-
+        Product productToDelete = tblProducts.getSelectionModel().getSelectedItem();
+        boolean isDeleted = Inventory.deleteProduct(productToDelete);
+        if (isDeleted){
+            tblProducts.setItems(Inventory.getAllProducts());
+        }
     }
 
     @FXML
@@ -108,18 +144,49 @@ public class MainController implements Initializable {
     }
 
     @FXML
-    void onActionAddPart(ActionEvent event) {
+    void onActionAddPart(ActionEvent event) throws IOException {
 
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/Views/AddPart.fxml"));
+        loader.load();
+
+        AddPartController controller = loader.getController();
+        controller.sendPartId(getNextPartId());
+
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        Parent scene = loader.getRoot();
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @FXML
-    void onActionModifyPart(ActionEvent event) {
+    void onActionModifyPart(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/Views/ModifyPart.fxml"));
+        loader.load();
 
+        ModifyPartController controller = loader.getController();
+        controller.sendPart(tblParts.getSelectionModel().getSelectedItem());
+
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        root = loader.getRoot();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
     void onActionDeletePart(ActionEvent event) {
+        Part partToDelete = tblParts.getSelectionModel().getSelectedItem();
+        boolean isDeleted = Inventory.deletePart(partToDelete);
+        if (isDeleted) {
+            tblParts.setItems(Inventory.getAllParts());
+        }
+    }
 
+    @FXML
+    void onActionExit(ActionEvent event){
+        System.exit(0);
     }
 
 
@@ -145,5 +212,27 @@ public class MainController implements Initializable {
         tblColumnProductName.setCellValueFactory(new PropertyValueFactory<>("name"));
         tblColumnProductInventory.setCellValueFactory(new PropertyValueFactory<>("stock"));
         tblColumnProductPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+    }
+
+    private int getNextPartId(){
+        int nextId = 0;
+        ObservableList<Part> parts = Inventory.getAllParts();
+        for (Part part : Inventory.getAllParts()
+             ) {
+                nextId = part.getId();
+        }
+        nextId++;
+        return nextId;
+    }
+
+    private int getNextProductId(){
+        int nextId = 0;
+        ObservableList<Product> products = Inventory.getAllProducts();
+        for (Product product : products
+             ) {
+            nextId  = product.getId();
+        }
+        nextId++;
+        return nextId;
     }
 }
