ee8111f4c9625a9068f6b91a8b2bfad3ac60e061
atho862 <54923852+atho862@users.noreply.github.com>
2020-06-22 23:29:03 -0500

    Implement onAction methods and add initialize methods

--- src/Controllers/ModifyProductController.java
+++ src/Controllers/ModifyProductController.java
@@ -1,7 +1,11 @@
 package Controllers;
 
+import DataProvider.Inventory;
 import Models.Part;
 import Models.Product;
+import Utilities.SearchUtility;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,15 +13,21 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
+import javax.swing.text.NumberFormatter;
 import java.io.IOException;
 import java.net.URL;
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class ModifyProductController implements Initializable {
     Stage stage;
     Parent root;
+    ObservableList<Part> associatedParts;
+    ObservableList<Part> filteredPartList;
 
     @FXML
     private TextField txtMin;
@@ -104,8 +114,22 @@ public class ModifyProductController implements Initializable {
     private TextField txtPrice;
 
     @FXML
-    void onActionSaveBtn(ActionEvent event) {
+    void onActionSaveBtn(ActionEvent event) throws IOException {
+        Product product = new Product(Integer.valueOf(txtId.getText()),
+                txtName.getText(),
+                Double.valueOf(txtPrice.getText()),
+                Integer.valueOf(txtInventory.getText()),
+                Integer.valueOf(txtMin.getText()),
+                Integer.valueOf(txtMax.getText()),
+                associatedParts);
+        int index = Inventory.getProductIndex(product.getId());
+        Inventory.updateProduct(index, product);
 
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("/Views/Main.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
@@ -119,31 +143,108 @@ public class ModifyProductController implements Initializable {
 
     @FXML
     void onActionSearchBtn(ActionEvent event) {
-
+        String searchString = txtSearch.getText();
+        if (SearchUtility.isStringNumeric(searchString)) {
+            Part part = Inventory.lookupPart(Integer.parseInt(searchString));
+            if (part == null) {
+                new Alert(Alert.AlertType.INFORMATION, String.format("Could not find part with Id %s.", searchString)).showAndWait();
+                return;
+            }
+            filteredPartList = FXCollections.observableArrayList();
+            filteredPartList.add(part);
+            tblViewAddAssociatedPart.setItems(filteredPartList);
+        }
+        else {
+            Part part = Inventory.lookupPart(searchString);
+            if (part == null) {
+                new Alert(Alert.AlertType.INFORMATION, String.format("Could not find part with name %s.", searchString)).showAndWait();
+                return;
+            }
+            filteredPartList = FXCollections.observableArrayList();
+            filteredPartList.add(part);
+            tblViewAddAssociatedPart.setItems(filteredPartList);
+        }
     }
 
     @FXML
     void onActionAddBtn(ActionEvent event) {
-
+        Part partToAdd = tblViewAddAssociatedPart.getSelectionModel().getSelectedItem();
+        if (associatedParts.contains(partToAdd)){
+            new Alert(Alert.AlertType.ERROR, "This part is already associated with the current product").showAndWait();
+            return;
+        }
+        associatedParts.add(partToAdd);
+        tblViewAssociatedParts.setItems(associatedParts);
     }
 
     @FXML
     void onActionDeleteBtn(ActionEvent event) {
-
+        Part partToDelete = tblViewAssociatedParts.getSelectionModel().getSelectedItem();
+        associatedParts.remove(partToDelete);
     }
 
     public void sendProduct(Product product){
+
         txtId.setText(String.valueOf(product.getId()));
         txtName.setText(product.getName());
         txtInventory.setText(String.valueOf(product.getStock()));
         txtPrice.setText(String.valueOf(product.getPrice()));
         txtMin.setText(String.valueOf(product.getMin()));
         txtMax.setText(String.valueOf(product.getMax()));
-        tblViewAssociatedParts.setItems(product.getAssociatedParts());
+        associatedParts = FXCollections.observableArrayList();
+        for (Part part : product.getAssociatedParts()
+             ) {
+            associatedParts.add(part);
+        }
+        tblViewAssociatedParts.setItems(associatedParts);
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        initializeAddAssociatedPartsTable();
+        initializeAssociatedPartsTable();
+    }
 
+    private void initializeAddAssociatedPartsTable(){
+        tblViewAddAssociatedPart.setItems(Inventory.getAllParts());
+        tblColumnAddPartId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblColumnAddPartName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tblColumnAddInventory.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        tblColumnAddPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+        tblColumnAddPrice.setCellFactory(tc -> new TableCell<Part, Double>(){
+            @Override
+            protected void updateItem(Double price, boolean empty) {
+                super.updateItem(price, empty);
+                if (empty){
+                    setText(null);
+                }
+                else {
+                    setText(currencyFormat.format(price));
+                }
+            }
+        });
+    }
+
+    private void initializeAssociatedPartsTable(){
+        associatedParts = tblViewAssociatedParts.getItems();
+        tblColumnPartId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblColumnPartName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tblColumnInventory.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        tblColumnPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+        tblColumnPrice.setCellFactory(tc -> new TableCell<Part, Double>(){
+            @Override
+            protected void updateItem(Double price, boolean empty) {
+                super.updateItem(price, empty);
+                if (empty){
+                    setText(null);
+                }
+                else {
+                    setText(currencyFormat.format(price));
+                }
+            }
+        });
     }
 }
